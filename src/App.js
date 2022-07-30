59e8fc3492021b2c2478c9b498f003785ced19dc
Puffy <88508824+PufflyMan@users.noreply.github.com>
2022-07-30 19:27:37 +0400

    bugged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
+import './style.css'
 
 export default function App() {
     const [darkMode, setDarkMode] = React.useState(true)
